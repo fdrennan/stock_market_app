cbaf2a4671f6299204dae8ee36d21745b6dd1825
Chris Evans <chris.evans@adroll.com>
2019-08-10 16:28:22 -0500

    Showing off dynamic routing

--- script1.py
+++ script1.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Homepage here!!"
+    return render_template('index.html')
+
+@app.route('/stats/<stat>')
+def stats(stat):
+    df = pd.read_csv('constituents-financials_csv.csv')
+    return render_template('stats.html',
+                           **locals())
+
 
 @app.route('/about/')
 def about():
     return "About content goes here!"
 
 if __name__ =="__main__":
-    app.run(debug = False)
+    app.run(debug = True)
