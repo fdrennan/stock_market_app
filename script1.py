73cce0012071e5ce56ba321b812b27afcb158ff9
LoweCoryR <LoweCoryR@gmail.com>
2019-08-16 08:37:48 -0500

    Revert "Showing off dynamic routing"
    This reverts commit cbaf2a4671f6299204dae8ee36d21745b6dd1825.

--- script1.py
+++ script1.py
@@ -1,22 +1,14 @@
-from flask import Flask, render_template
-import pandas as pd
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/stats/<stat>')
-def stats(stat):
-    df = pd.read_csv('constituents-financials_csv.csv')
-    return render_template('stats.html',
-                           **locals())
-
+    return "Homepage here!!"
 
 @app.route('/about/')
 def about():
     return "About content goes here!"
 
 if __name__ =="__main__":
-    app.run(debug = True)
+    app.run(debug = False)
